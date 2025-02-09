a9e85858d7f65893955c5ac112155c4cbd3c43a5
tifan9 <uwasesophie91@gmail.com>
2025-02-09 19:33:27 +0200

    spring security

---
+++ src/main/java/com/auca/usereg/UseRegApplication.java
@@ -0,0 +1,13 @@
+package com.auca.usereg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseRegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseRegApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/auca/usereg/UseRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auca.usereg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseRegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
